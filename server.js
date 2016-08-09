85f1c3211b1bda5ec1dcd483a7518971ae7f1305
Kan Adachi <Kadachi1023@gmail.com>
2016-08-09 12:20:32 -0700

    [first] first commit

---
+++ server.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var app = express();
+var os = require('os').homedir();
+
+var LEX = require('letsencrypt-express').testing();
+
+// Change these two lines!
+var DOMAIN = 'localhost';
+var EMAIL = 'kan@kanadachi.com';
+
+var lex = LEX.create({
+  configDir: require('os').homedir() + '/letsencrypt/etc'
+, approveRegistration: function (hostname, approve) { // leave `null` to disable automatic registration
+    if (hostname === DOMAIN) { // Or check a database or list of allowed domains
+      approve(null, {
+        domains: [DOMAIN]
+      , email: EMAIL
+      , agreeTos: true
+      });
+    }
+  }
+});
+
+app.use(express.static('./client'));
+
+lex.onRequest = app;
+
+lex.listen([80], [443, 5001], function () {
+  var protocol = ('requestCert' in this) ? 'https': 'http';
+  console.log("Listening at " + protocol + '://localhost:' + this.address().port);
+});
